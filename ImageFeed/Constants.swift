a5576e9cc5ed2316bf7b25392a379078f1464d66
Alex Petrenko <alex.pt@ya.ru>
2024-01-01 17:54:57 +0300

    add Constants.swift

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "J_EGyhNNQkqVZSqCLiCSbFV-94-F3I5gzuV__hCCiVg"
+let SecretKey = "b0OJYfZdImV2xGoi0hrw8kGNZpa16kGv64DYDnW0-20"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")
